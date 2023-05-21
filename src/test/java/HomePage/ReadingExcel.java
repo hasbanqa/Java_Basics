c5e25b77e5dd085b4f0eabfe65c666198a406f60
hasbanqat <75902806+hasbanqat@users.noreply.github.com>
2023-05-21 12:41:00 -0400

    Apache poi1

--- src/test/java/HomePage/ReadingExcel.java
+++ src/test/java/HomePage/ReadingExcel.java
@@ -8,6 +8,7 @@ package HomePage;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,17 +17,28 @@ public class ReadingExcel {
 
     public static void main(String[] args) throws IOException {
 
-        FileInputStream file = new FileInputStream("C:\\Users\\hasba\\IdeaProjects\\com.OrangeHRM\\testdata\\testdata.xlsx");
-//        FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"\\testdata\\testdata.xlsx");
+////        FileInputStream file = new FileInputStream("C:\\Users\\hasba\\IdeaProjects\\com.OrangeHRM\\testdata\\data.xlsx");
+//        FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"\\testdata\\data.xlsx");
+//
+//        XSSFWorkbook workbook = new XSSFWorkbook(file);
+//        XSSFSheet sheet=workbook.getSheet("Sheet1");
+//
+//        int totalrows=sheet.getLastRowNum();
+//        int totalcells=sheet.getRow(1).getLastCellNum();
+//
+//        System.out.println(totalrows);
+//        System.out.println(totalcells);
 
-        XSSFWorkbook workbook = new XSSFWorkbook(file);
-        XSSFSheet sheet=workbook.getSheet("Sheet1");
+        File src= new File(".\\testdata\\data.xlsx");
 
-        int totalrows=sheet.getLastRowNum();
-        int totalcells=sheet.getRow(1).getLastCellNum();
+        FileInputStream fis = new FileInputStream(src);
+        XSSFWorkbook xsf = new XSSFWorkbook(fis);
+        XSSFSheet sheet = xsf.getSheetAt(0);
+        String entry1= sheet.getRow(1).getCell(1).getStringCellValue();
+        System.out.println(entry1);
 
-        System.out.println(totalrows);
-        System.out.println(totalcells);
+
+
 
     }
 
